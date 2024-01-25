61216f82cb816525e70ab7a610a43f98d2ada6a8
Muhamadsahwal <nurjamansahwal@gmail.com>
2024-01-25 14:07:29 +0700

    argumrnt

---
+++ js/argument.js
@@ -0,0 +1,13 @@
+// Tulis kode kalian di bawah ini
+
+function luasPersegi(s) {
+
+    return s * s; // Menghitung luas persegi dengan rumus sisi * sisi
+  
+  }
+  
+  // Contoh penggunaan fungsi dengan sisi 4
+  
+  let sisi = 4;
+  
+  console.log("Luas persegi dengan sisi", sisi, "adalah:", luasPersegi(sisi));
